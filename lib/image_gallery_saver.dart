8c901919dc20344c1b450ece756bf64e3b1b829b
林泽池 <linzechi@deepcoin.info>
2022-07-19 13:43:26 +0800

    适配kotlin 依赖高版本

--- lib/image_gallery_saver.dart
+++ lib/image_gallery_saver.dart
@@ -4,8 +4,7 @@ import 'dart:typed_data';
 import 'package:flutter/services.dart';
 
 class ImageGallerySaver {
-  static const MethodChannel _channel =
-      const MethodChannel('image_gallery_saver');
+  static const MethodChannel _channel = const MethodChannel('image_gallery_saver');
 
   /// save image to Gallery
   /// imageBytes can't null
